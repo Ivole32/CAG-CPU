f4f98d1225023e70071570387bef37ba08f5cd9a
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2025-02-19 16:26:30 +0100

    Create gates.py

---
+++ gates.py
@@ -0,0 +1,15 @@
+def AND(a, b):
+    if a == 1 and b == 1:
+        return 1
+    else:
+        return 0
+    
+def OR(a, b):
+    if a == 1 or b == 1:
+        return 1
+    
+def XOR(a, b):
+    if a == b:
+        return 0
+    else:
+        return 1
